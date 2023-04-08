a77262e8ef505834345ee12a1a37c6ca345cbb98
Zhivko Zhelev <zh.zhelev@tinqin.com>
2023-04-08 17:55:20 +0300

    Created methods create and getSalesByDateRange

--- src/main/java/com/pros/parkinglot/services/SaleService.java
+++ src/main/java/com/pros/parkinglot/services/SaleService.java
@@ -1,5 +1,14 @@
 package com.pros.parkinglot.services;
 
+import com.pros.parkinglot.models.Sale;
+
+import java.time.LocalDate;
+import java.util.List;
+
 public interface SaleService {
+
+	Sale create(Sale sale);
+
+	List<Sale> getSalesByDateRange(LocalDate startDate, LocalDate endDate);
 
 }
--- src/main/java/com/pros/parkinglot/services/SaleServiceImpl.java
+++ src/main/java/com/pros/parkinglot/services/SaleServiceImpl.java
@@ -1,10 +1,23 @@
 package com.pros.parkinglot.services;
 
+import com.pros.parkinglot.models.Sale;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Service
 @Transactional
 public class SaleServiceImpl implements SaleService {
 
+	@Override
+	public Sale create(Sale sale) {
+		return null;
+	}
+
+	@Override
+	public List<Sale> getSalesByDateRange(LocalDate startDate, LocalDate endDate) {
+		return null;
+	}
 }
