eb7bf790c7657ff7e817e531cddaad876735f66f
Zhivko Zhelev <zh.zhelev@tinqin.com>
2023-04-08 22:32:03 +0300

    Updated enter and exit methods

--- src/main/java/com/pros/parkinglot/controllers/ParkingLotController.java
+++ src/main/java/com/pros/parkinglot/controllers/ParkingLotController.java
@@ -1,16 +1,14 @@
 package com.pros.parkinglot.controllers;
 
+import com.pros.parkinglot.models.Sale;
 import com.pros.parkinglot.models.Ticket;
 import com.pros.parkinglot.models.Vehicle;
 import com.pros.parkinglot.services.ParkingLotService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.math.BigDecimal;
 
 @RestController
 @RequestMapping("/parking")
@@ -24,13 +22,13 @@ public class ParkingLotController {
 	}
 
 	@PostMapping("/enter")
-	public ResponseEntity<Ticket> enter(@RequestParam Vehicle vehicle) {
-		return ResponseEntity.ok(null);
+	public Ticket enter(@RequestParam Vehicle vehicle) {
+		return null;
 	}
 
 	@PostMapping("/exit")
-	public ResponseEntity<BigDecimal> exit(@RequestParam Long id) {
-		return ResponseEntity.ok(null);
+	public Sale exit(@RequestParam Long id) {
+		return null;
 	}
 }
 
