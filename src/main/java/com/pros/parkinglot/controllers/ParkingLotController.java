3496d00b952e877583d0f583f87503227bf748b4
Zhivko Zhelev <zh.zhelev@tinqin.com>
2023-04-09 17:24:30 +0300

    Created RestControllerExceptionHandler to handle exceptions in the rest controllers

--- src/main/java/com/pros/parkinglot/controllers/ParkingLotController.java
+++ src/main/java/com/pros/parkinglot/controllers/ParkingLotController.java
@@ -2,15 +2,22 @@ package com.pros.parkinglot.controllers;
 
 import com.pros.parkinglot.models.Sale;
 import com.pros.parkinglot.models.Ticket;
-import com.pros.parkinglot.models.Vehicle;
+import com.pros.parkinglot.models.VehicleType;
 import com.pros.parkinglot.services.ParkingLotService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.constraints.Pattern;
+import java.util.Optional;
+
 @RestController
+@Validated
 @RequestMapping("/parking")
 public class ParkingLotController {
 
@@ -22,13 +29,24 @@ public class ParkingLotController {
 	}
 
 	@PostMapping("/enter")
-	public Ticket enter(@RequestParam Vehicle vehicle) {
-		return null;
+	public ResponseEntity<Optional<Ticket>> enter(
+			@RequestParam @Pattern(regexp = "^[A-Z0-9-]*$") String plateNumber,
+			@RequestParam @Pattern(regexp = "^(bus|car)$") String vehicleType) {
+		boolean hasAvailableSpot = parkingLotService.checkIfSpotIsAvailable(VehicleType.valueOf(vehicleType.toUpperCase()));
+		return hasAvailableSpot
+				? ResponseEntity.ok().body(Optional.of(parkingLotService.enter(plateNumber)))
+				: ResponseEntity.status(HttpStatus.CONFLICT).body(Optional.empty());
 	}
 
 	@PostMapping("/exit")
 	public Sale exit(@RequestParam Long id) {
+		// Calculate the sale total based on the parking duration and vehicle type
+		// Save the sale record in the database and free up the parking spot
 		return null;
+	}
+
+	private static VehicleType convertToVehicleType(String vehicleType) {
+		return VehicleType.valueOf(vehicleType.toUpperCase());
 	}
 }
 
