58cc4a821e62e21adc52f66bb44c347cbf1078b8
Zhivko Zhelev <zh.zhelev@tinqin.com>
2023-04-09 17:24:59 +0300

    Added validations of plateNumber

--- src/main/java/com/pros/parkinglot/models/Vehicle.java
+++ src/main/java/com/pros/parkinglot/models/Vehicle.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 @Getter
 @Setter
@@ -18,6 +20,8 @@ public class Vehicle {
 	@Enumerated(EnumType.STRING)
 	private VehicleType vehicleType;
 
-	private String licenseNumber;
+	@NotNull(message = "Plate number is required")
+	@Pattern(regexp = "^[A-Z0-9-]*$")
+	private String plateNumber;
 
 }
