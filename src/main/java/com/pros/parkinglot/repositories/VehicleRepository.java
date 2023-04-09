69750a0aec2da49a2c44f89efb4ddaffcb99d4bf
Zhivko Zhelev <zh.zhelev@tinqin.com>
2023-04-09 19:59:46 +0300

    Updated enter method. Now it saves the vehicle and ticket in the database

---
+++ src/main/java/com/pros/parkinglot/repositories/VehicleRepository.java
@@ -0,0 +1,13 @@
+package com.pros.parkinglot.repositories;
+
+import com.pros.parkinglot.models.Vehicle;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
+
+	Vehicle save(Vehicle ticket);
+
+}
+
--- src/main/java/com/pros/parkinglot/services/ParkingLotServiceImpl.java
+++ src/main/java/com/pros/parkinglot/services/ParkingLotServiceImpl.java
@@ -1,9 +1,11 @@
 package com.pros.parkinglot.services;
 
 import com.pros.parkinglot.models.Ticket;
+import com.pros.parkinglot.models.TicketStatus;
 import com.pros.parkinglot.models.Vehicle;
 import com.pros.parkinglot.models.VehicleType;
 import com.pros.parkinglot.repositories.TicketRepository;
+import com.pros.parkinglot.repositories.VehicleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,19 +17,30 @@ import java.time.LocalDateTime;
 public class ParkingLotServiceImpl implements ParkingLotService {
 
 	private final TicketRepository ticketRepository;
+	private final VehicleRepository vehicleRepository;
 
 	@Autowired
-	public ParkingLotServiceImpl(TicketRepository ticketRepository) {
+	public ParkingLotServiceImpl(TicketRepository ticketRepository, VehicleRepository vehicleRepository) {
 		this.ticketRepository = ticketRepository;
+		this.vehicleRepository = vehicleRepository;
 	}
 
 	@Override
 	public Ticket enter(String plateNumber, VehicleType vehicleType) {
 		Ticket ticket = new Ticket();
 		ticket.setEntryTime(LocalDateTime.now());
+		ticket.setStatus(TicketStatus.ACTIVE);
+		Vehicle vehicle = getVehicle(plateNumber, vehicleType);
+		ticket.setVehicle(vehicle);
+
+		vehicleRepository.save(vehicle);
+		return ticketRepository.save(ticket);
+	}
+
+	private static Vehicle getVehicle(String plateNumber, VehicleType vehicleType) {
 		Vehicle vehicle = new Vehicle();
 		vehicle.setPlateNumber(plateNumber);
-		ticket.setVehicle(vehicle);
-		return ticketRepository.save(ticket);
+		vehicle.setVehicleType(vehicleType);
+		return vehicle;
 	}
 }
