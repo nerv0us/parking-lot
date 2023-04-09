4904125fd1ab9827f854507463792a199453c9a1
Zhivko Zhelev <zh.zhelev@tinqin.com>
2023-04-09 19:56:56 +0300

    Removed ticket

--- src/main/java/com/pros/parkinglot/models/Vehicle.java
+++ src/main/java/com/pros/parkinglot/models/Vehicle.java
@@ -5,7 +5,6 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
 
 @Getter
 @Setter
@@ -20,11 +19,7 @@ public class Vehicle {
 	@Enumerated(EnumType.STRING)
 	private VehicleType vehicleType;
 
-	@NotNull(message = "Plate number is required")
-	@Pattern(regexp = "^[A-Z0-9-]*$")
+	@NotNull
 	private String plateNumber;
-
-	@OneToOne
-	private Ticket ticket;
 
 }
