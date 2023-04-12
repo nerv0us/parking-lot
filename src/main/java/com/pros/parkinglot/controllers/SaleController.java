bde613b4f6f48f5e624637b7d569b5dab0a8b877
Zhivko Zhelev <zh.zhelev@tinqin.com>
2023-04-12 14:44:22 +0300

    Added api in the request mapping. Added a monthly report get method

--- src/main/java/com/pros/parkinglot/controllers/SaleController.java
+++ src/main/java/com/pros/parkinglot/controllers/SaleController.java
@@ -4,13 +4,17 @@ import com.pros.parkinglot.models.sale.Sale;
 import com.pros.parkinglot.services.sale.SaleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
+import java.time.YearMonth;
 import java.util.List;
 
 @RestController
-@RequestMapping("/sales")
+@RequestMapping("/api/sales")
 public class SaleController {
 
 	private final SaleService saleService;
@@ -20,11 +24,6 @@ public class SaleController {
 		this.saleService = saleService;
 	}
 
-	@PostMapping("/create")
-	public Sale create(@RequestBody Sale sale) {
-		return saleService.create(sale);
-	}
-
 	@GetMapping("/report")
 	public List<Sale> report(
 			@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
@@ -32,11 +31,10 @@ public class SaleController {
 		return saleService.getSalesByDateRange(startDate, endDate);
 	}
 
-	//	@GetMapping("/report")
-	//	public List<Sale> monthlyReport(@RequestParam @DateTimeFormat(pattern = "yyyy-MM") YearMonth yearMonth) {
-	//		LocalDate startDate = yearMonth.atDay(1);
-	//		LocalDate endDate = yearMonth.atEndOfMonth();
-	//		return saleService.getSalesByDateRange(startDate, endDate);
-	//	}
+	@GetMapping("/monthlyReport")
+	public List<Sale> monthlyReport(@RequestParam @DateTimeFormat(pattern = "yyyy-MM") YearMonth yearMonth) {
+		LocalDate startDate = yearMonth.atDay(1);
+		LocalDate endDate = yearMonth.atEndOfMonth();
+		return saleService.getSalesByDateRange(startDate, endDate);
+	}
 }
-
