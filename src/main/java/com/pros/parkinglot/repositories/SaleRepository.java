7022c58001fa0bea1417fa6e538682fafd3ea574
Zhivko Zhelev <zh.zhelev@tinqin.com>
2023-04-13 13:38:41 +0300

    Removed save method

--- src/main/java/com/pros/parkinglot/repositories/SaleRepository.java
+++ src/main/java/com/pros/parkinglot/repositories/SaleRepository.java
@@ -15,18 +15,4 @@ public interface SaleRepository extends JpaRepository<Sale, Long> {
 	@Query("SELECT s FROM Sale s WHERE s.date BETWEEN :startDate AND :endDate")
 	List<Sale> findInRange(@Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate);
 
-	Sale save(Sale sale);
-
-	//	@PersistenceContext
-	//	private EntityManager entityManager;
-	//
-	//	public List<Sale> findSalesByDateRange(LocalDate startDate, LocalDate endDate) {
-	//		String sql = "SELECT s FROM Sale s WHERE s.date BETWEEN :startDate AND :endDate";
-	//		Query query = entityManager.createQuery(sql, Sale.class);
-	//		query.setParameter("startDate", startDate);
-	//		query.setParameter("endDate", endDate);
-	//		List<Sale> sales = query.getResultList();
-	//		return sales;
-	//	}
-
 }
--- src/main/java/com/pros/parkinglot/repositories/TicketRepository.java
+++ src/main/java/com/pros/parkinglot/repositories/TicketRepository.java
@@ -7,7 +7,4 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
 
-	Ticket save(Ticket ticket);
-
 }
-
